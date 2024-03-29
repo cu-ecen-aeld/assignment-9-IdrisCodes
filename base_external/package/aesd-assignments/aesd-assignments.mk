124e6b2e9a8a9948280b12a9909ad18d76c1e645
Mohamed Idris <Mohamed.Idris.Farhani@outlook.com>
2024-03-29 16:46:52 +0100

    Assigment-9-Done

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cf424482d65a4017a041c824afa16e0c9b2b20e6'
+AESD_ASSIGNMENTS_VERSION = '8411b86921f3a55fe637bbaca1b42b4bb0c70664'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,18 +5,18 @@
 ################################################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DRIVER_VERSION = 'cf424482d65a4017a041c824afa16e0c9b2b20e6'
+AESDCHAR_VERSION = '8411b86921f3a55fe637bbaca1b42b4bb0c70664'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_CHAR_DRIVER_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-IdrisCodes.git'
-AESD_CHAR_DRIVER_SITE_METHOD = git
-AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+AESDCHAR_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-IdrisCodes.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
 
-AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver/
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver/
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
 endef
